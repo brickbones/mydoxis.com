46e0db12b677725fd7ed456086f208498c9a686b
David Ossa Hernández <david@hombrelobo.co>
2021-05-19 16:30:06 -0500

    Corrección en texto del about us

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -23,11 +23,6 @@ export default function AboutUs({ helpers }) {
               ? 'DOXIS name is a wordplay that has Spanish references, since GOD in Spanish translates DIOS and the X in the middle means mystery-higher power then DOXIS stands for God-music-art-life. DOXIS logo is a test tube with the "eye of God" in it. It reminds us to never give up in our dreams, also represents our willingness to experiment new techniques to reach our goals.'
               : 'El nombre de DOXIS es un juego de palabras en español donde DIOS mas la letra X conforman la palabra DOXIS así que DOXIS significa: Dios-música-arte-vida. El logo de DOXIS es un tubo de ensayo con el "ojo de Dios" para recordarnos de nunca renunciar a nuestros sueños, también representa probar nuevas técnicas para lograr nuestras metas.'}
           </p>
-          {/* <p className='mb-20 text-gray-900 max-w-prose text-lg'>
-            {locale === 'en'
-              ? 'DOXIS was born in 2012 somewhere in Puerto Rico USA and somewhere in Medellin Colombia when Jowell & Randy, the Puerto Rican duo that changed everything for good in the reggaeton music, joined fashion designers stylists and entrepreneurs Michael Clements & Paula Sanchez, both have worked previously for the first successful reggaeton clothing brand AKOLATRONIC and also had work with DJ NELSON in another project: called "Angels" for WISIN y Yandel.'
-              : 'DOXIS en el año 2012 en algún lugar de Puerto Rico USA y en algún lugar de Medellín Colombia cuando Jowell & Randy, el dúo puertorriqueño, se unen a Michael Clements & Paula Sánchez diseñadores, estilistas y empresarios que ya habían trabajado en la primera marca exitosa de reggaetón: AKOLATRONIC y también habían trabajado con DJ Nelson en otro proyecto de marca llamado "Angels" para WISIN y Yandel.'}
-          </p> */}
           <div className='mb-20 md:flex md:justify-between max-w-prose'>
             <div className='flex-1 mb-10 text-lg'>
               <h4 className='mb-5 text-lg font-semibold'>
